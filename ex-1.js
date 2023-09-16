7f89fbd3184034b15b41c51f3ade5218df623208
Natkamon Saelim <pim-i-smart@hotmail.com>
2023-09-16 20:03:42 +0700

    fix: fix bug in assignment by pim

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,8 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
 
+//แก้ไข Bug
+depositAmountFromUser = Number(depositAmountFromUser);
 accountBalance = accountBalance + depositAmountFromUser;
-
-let accountBalanceMessage = "Your account balance is " + accountBalance;
-
+let accountBalanceMessage = `Your account balance is ${accountBalance}`;
 console.log(accountBalanceMessage);
